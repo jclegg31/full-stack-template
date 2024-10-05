8a46d01546d6d15e4bf0b517d621b5a97e20ee96
julie_c <forechecker@gmail.com>
2024-10-05 14:46:29 -0600

    added variables and mongo connection

--- server.js
+++ server.js
@@ -4,4 +4,15 @@ const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config() 
  
+let db, 
+    dbConnectionString = process.env.DB_STRING, //gets string from env file
+    dbName = 'star-trek-api',
+    collection
 
+//just establishing connection, not sending data back and forth so you don't need req, res
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('connected to da database')
+        db = client.db(dbName)
+        collection = db.collection('alien-info')
+    })
